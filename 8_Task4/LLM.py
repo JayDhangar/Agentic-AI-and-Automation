8ef5c2584706ff59e829401c7850f213590bcbf4
Jay Dhangar <jaydhangar2001@gmail.com>
2026-01-26 19:15:01 +0530

    Update gitignore and LLM logic

--- 8_Task4/LLM.py
+++ 8_Task4/LLM.py
@@ -3,7 +3,9 @@ from langchain_core.output_parsers import PydanticOutputParser
 from Schemas import ResumeSchema, SkillMatch
 from prompts import resume_prompt, skill_prompt
 from dotenv import load_dotenv
+
 load_dotenv()
+
 llm = ChatGroq(model="llama-3.1-8b-instant", temperature=0)
 
 resume_parser = PydanticOutputParser(pydantic_object=ResumeSchema)
